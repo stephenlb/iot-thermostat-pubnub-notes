39f32b6b2f77c0af659a4421adb2440675985fdb
Stephen Blum <blum.stephen@gmail.com>
2021-10-13 20:02:41 -0700

    one last test, then we will add network calls.

--- thermostat/network.js
+++ thermostat/network.js
@@ -1,15 +1,25 @@
 class Network {
     constructor() {
+        this.channels = [];
+        this.origin = 'ps.pndsn.com';
+        this.pubkey = '';
         this.subkey = '';
-        this.pubnub = PubNub({});
+        this.pubnub = PubNub({
+            origin: this.origin,
+            pubkey: this.pubkey,
+            subkey: this.subkey,
+        });
     }
 
-    login() {
+    login(email, password) {
+        let url = `https://${this.origin}/`;
     }
 
     provision() {
+        let url = `https://${this.origin}/`;
     }
 
     connect() {
+        let url = `https://${this.origin}/`;
     }
 }
--- thermostat/ui.js
+++ thermostat/ui.js
@@ -15,8 +15,8 @@ const body = document.querySelector('body');
 const hot  = document.querySelector('#set div.hot');
 const cold = document.querySelector('#set div.cold');
 
-body.addEventListener('click', startSound, {passive: false});
-body.addEventListener('touchend', startSound, {passive: false});
+body.addEventListener('click', readySound, {passive: false});
+body.addEventListener('touchend', readySound, {passive: false});
 body.addEventListener('mousedown', dragStart, {passive: false});
 body.addEventListener('touchstart', dragStart, {passive: false});
 body.addEventListener('mousemove', drag, {passive: false});
@@ -45,9 +45,11 @@ function drag(event) {
 
     if (sound) sound.play(240.0, 'sine', 0.2);
 }
-function startSound() {
+function readySound() {
     event.preventDefault(); 
-    if (!sound) sound = new Sound(); 
+    if (sound) return;
+    sound = new Sound(); 
+    document.documentElement.requestFullscreen();
 }
 
 const temperatureDisplay = document.querySelector('#temperature');
